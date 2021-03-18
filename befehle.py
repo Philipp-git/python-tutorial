300a1072c9d2fc3e0e611322c2cbca613124b40e
Philipp-git <70282784+Philipp-git@users.noreply.github.com>
2021-03-18 11:00:36 +0100

    Update befehle.py

--- befehle.py
+++ befehle.py
@@ -2,4 +2,5 @@ print(text)
 #zeigt einen text im terminal
 
 #variablen
-Integer = 1 #gamzzahl von -2147483648 bis 2147483647
+Integer = 1 #  Ganzzahl von -2147483648 bis 2147483647
+string = "ein text" # Zeichenkette unentlich lang
