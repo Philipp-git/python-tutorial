ba8f20e19276287823544c0d1be200063a1a5c35
Philipp-git <70282784+Philipp-git@users.noreply.github.com>
2021-03-18 11:12:18 +0100

    Update befehle.py

--- befehle.py
+++ befehle.py
@@ -1,6 +1,13 @@
-print(text)
-#zeigt einen text im terminal
-
 #variablen
 Integer = 1 #  Ganzzahl von -2147483648 bis 2147483647
-string = "ein text" # Zeichenkette unentlich lang
+float = 3.141592 #kommerzahl
+string = "ein text" # Zeichenkette (unentlich lang)
+boolean = True #kann True oder False sein
+list = [element0,element1,elememt2] # kann verschiedene Variablen speichern mit list[index] zugreifen
+
+print(text) # ->none
+#zeigt einen text im terminal
+
+input(frage) # ->return antwort
+#pausiert das skript bis eine antwort eingegeben wurde.
+
